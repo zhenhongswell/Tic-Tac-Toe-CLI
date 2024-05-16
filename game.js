0a4c2371a1f66e0ac523fd918754b3ce6ee9ef18
zhenhongswell <zhenhongswell@gmail.com>
2024-05-16 14:07:28 +0800

    [js] Add game core logic

--- game.js
+++ game.js
@@ -1,7 +1,124 @@
-function sum(a, b) {
-  return a + b;
+const EMPTY = "";
+const P1 = "X";
+const P2 = "O";
+const boardSize = 2;
+const currentPlayer = "";
+const state = [[], [], []];
+const position = { row: null, col: null };
+function createEmptyBoard() {
+  return [
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+  ];
 }
 
+function updateGame(player, position, state) {
+  // default
+  const i = position[0];
+  const j = position[1];
+  let sucess = false;
+  if (i < 0 || i >= state.length) {
+    return [state, false];
+  }
+  if (j < 0 || j > state[0].length) {
+    return [state, false];
+  }
+  if (state[i][j] !== EMPTY) {
+    return [state, false];
+  }
+  const newState = state.map((row, rowIndex) => {
+    if (rowIndex === i) {
+      return row.map((cell, cellIndex) => {
+        if (cellIndex === j) {
+          return player;
+        }
+        return cell;
+      });
+    }
+    return row;
+  });
+
+  return [newState, true];
+}
+
+function checkWinner(state) {
+  const winner = checkRows(state) || checkCols(state) || checkDiags(state);
+  if (winner !== null) {
+    return winner;
+  }
+  if (isBoardFull(state)) {
+    return "draw";
+  }
+  return null;
+}
+
+function checkRows(state) {
+  for (let i = 0; i < state.length; i++) {
+    const row = state[i];
+    const winner = checkRow(row);
+    if (winner !== null) {
+      return winner;
+    }
+  }
+  return null;
+}
+function checkRow(row) {
+  const first = row[0];
+  if (first === EMPTY) {
+    return null;
+  }
+  for (const cell of row) {
+    if (cell !== first) {
+      return null;
+    }
+  }
+  return first;
+}
+
+function checkCols(state) {
+  for (let i = 0; i < state.length; i++) {
+    const col = state.map((row) => {
+      return row[i];
+    });
+    const winner = checkRow(col);
+    if (winner !== null) {
+      return winner;
+    }
+  }
+  return null;
+}
+
+function checkDiags(state) {
+  const diag1 = state.map((row, rowIndex) => {
+    return row[rowIndex];
+  });
+  const diag2 = state.map((row, rowIndex) => {
+    return row[row.length - 1 - rowIndex];
+  });
+  return checkRow(diag1) || checkRow(diag2);
+}
+
+function isBoardFull(state) {
+  for (const row of state) {
+    for (const col of row) {
+      if (col === EMPTY) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// function checkCols(state){
+//   for
+// }
+
 module.exports = {
-  sum,
+  createEmptyBoard,
+  updateGame,
+  checkWinner,
+  EMPTY,
+  P1,
+  P2,
 };
--- game.spec.js
+++ game.spec.js
@@ -1,14 +1,197 @@
 // to test game.js function
-const { sum } = require("./game.js");
+const {
+  createEmptyBoard,
+  EMPTY,
+  updateGame,
+  P1,
+  P2,
+  checkWinner,
+} = require("./game.js");
 
-describe("sum", () => {
-  test("1+1", () => {
-    expect(sum(1, 1)).toBe(2);
+describe("createEmptyBoard", () => {
+  test("create empty board", () => {
+    const broad = createEmptyBoard();
+    expect(broad).toEqual([
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
   });
-  test("1+2", () => {
-    expect(sum(1, 2)).toBe(3);
+});
+
+describe("updateGame", () => {
+  test("update sucess", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const [newState, sucess] = updateGame(P2, [1, 1], state);
+    expect(sucess).toBe(true);
+    expect(newState).toEqual([
+      [P1, EMPTY, EMPTY],
+      [EMPTY, P2, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
   });
-  test("-100+3", () => {
-    expect(sum(-100, 3)).toBe(-97);
+
+  test("update failure", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const [newState, sucess] = updateGame(P2, [0, 0], state);
+    expect(sucess).toBe(false);
+    expect(newState).toEqual([
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
+  });
+
+  test("out of bounds", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const [newState, sucess] = updateGame(P2, [3, 3], state);
+    expect(sucess).toBe(false);
+    expect(newState).toEqual([
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
+  });
+});
+
+describe("checkWinner", () => {
+  const state = [];
+  test("Top row wins", () => {
+    const state = [
+      [P1, P1, P1],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("Middle row wins", () => {
+    const state = [
+      [EMPTY, EMPTY, EMPTY],
+      [P1, P1, P1],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("Bottom row wins", () => {
+    const state = [
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [P1, P1, P1],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("No winner 1", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, P2, EMPTY],
+      [P1, EMPTY, P2],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(null);
+  });
+  test("No winner 2", () => {
+    const state = [
+      [P2, P1, P1],
+      [EMPTY, P1, EMPTY],
+      [EMPTY, EMPTY, P2],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(null);
+  });
+  test("No winner 3", () => {
+    const state = [
+      [P2, P1, P2],
+      [EMPTY, P2, EMPTY],
+      [P1, EMPTY, P1],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(null);
+  });
+  test("Diag wins 1", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, P1, EMPTY],
+      [P2, P2, P1],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("Diag wins 2", () => {
+    const state = [
+      [EMPTY, EMPTY, P1],
+      [EMPTY, P1, P2],
+      [P1, P2, EMPTY],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("Diag wins 3", () => {
+    const state = [
+      [EMPTY, EMPTY, P2],
+      [EMPTY, P2, P1],
+      [P2, P1, P1],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P2);
+  });
+  test("col 0 wins", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [P1, EMPTY, EMPTY],
+      [P1, EMPTY, EMPTY],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("col 0 wins", () => {
+    const state = [
+      [P1, P2, EMPTY],
+      [P1, P2, EMPTY],
+      [P1, EMPTY, EMPTY],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("col 1 wins", () => {
+    const state = [
+      [P2, P1, EMPTY],
+      [P2, P1, EMPTY],
+      [EMPTY, P1, EMPTY],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("col 2 wins", () => {
+    const state = [
+      [EMPTY, P2, P1],
+      [EMPTY, P2, P1],
+      [EMPTY, EMPTY, P1],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe(P1);
+  });
+  test("Draw", () => {
+    const state = [
+      [P1, P1, P2],
+      [P2, P2, P1],
+      [P1, P2, P1],
+    ];
+    const winner = checkWinner(state);
+    expect(winner).toBe("draw");
   });
 });
